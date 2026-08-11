de5f188703cc66b2f9b7b33d51273379f9de1c7a
kosivivian <kosisochukwu.nebolisa@pau.edu.ng>
2026-08-11 15:14:38 +0100

    made changes

--- app.py
+++ app.py
@@ -49,9 +49,16 @@ from helpers.llm_chain import create_rag_chain
 # ENVIRONMENT
 # =============================================================
 
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    GROQ_API_KEY = st.secrets.get("GROQ_API_KEY")
 
 
 # =============================================================
